65eae16324e4af1196a224493f32b6bf75f974be
Jaki Widby <jaki.widby@gmail.com>
2020-08-12 14:12:53 -0700

    append values to specific ids on webpage

--- js/scripts.js
+++ js/scripts.js
@@ -5,5 +5,14 @@ $(document).ready(function() {
     const address = $("input#address").val();
     const item = $("#item").val();
     const color = $("#color").val();
+    event.preventDefault();
+
+    $("#receipt #name").text("Name: " + firstName + " " + lastName);
+    $("#receipt #address").text("Address: " + address);
+    $("#receipt #itemInfo").children("li").remove();
+    $("#receipt #itemInfo").append("<li>Item ordered: " + color +" "+ item +"</li>");
+    $("#thanks").text(("Thank you for your order!"));
+    $("#receipt").show();
+  
   });
 });
